13b73ff28b0bced867749d0b222ee43abf06d595
unknown <you@example.com>
2025-07-31 20:44:39 +0200

    playwright first lection

---
+++ tests/learning/first-scripts/first_tests.spec.ts
@@ -0,0 +1,23 @@
+import { test } from "@playwright/test";
+
+test("First test", async ({ page }) => {
+  //test code
+  await page.goto("http://tredgate.com/pmtool/");
+  await page.locator("#username").fill("pw_academy");
+  await page.locator("#password").fill("Playwright321!");
+  await page.locator(".btn").click();
+});
+
+//pwt - zkratka kdy vyberu automatický začátek testu a kliknu na pwt-test
+
+// ! užitečné příkazy do terminálu
+/*playwright runner for ui příkaz: npx playwright test --ui
+
+
+otevřít test report: 
+npx playwright show-report
+
+spuštění testů - všech: npx playwright test first_tests.spec.ts
+
+pokud mám více prohlížečů a chci spustit jen v jednom prohlížeči test příkaz: npx playwright test first_tests.spec.ts --project=firefo
+*/
