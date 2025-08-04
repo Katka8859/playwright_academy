39f72539b080789361a6ce023cba03b50b2bc225
unknown <you@example.com>
2025-08-04 14:42:39 +0200

    Homework:Easy playwright test

---
+++ tests/homework/hw_intro_playwright.spec.ts
@@ -0,0 +1,11 @@
+import { test } from "@playwright/test";
+
+test("add product to card", async ({ page }) => {
+  //test code
+  await page.goto("https://tredgate.com/eshop/");
+  await page.locator("[name='search']").fill("iPhone");
+  await page.locator("#search [type='button']").click();
+  await page.locator("//a[text()='iPhone']").click();
+  await page.locator("#button-cart").click();
+  await page.waitForTimeout(2000); //doplněno do testu z důvodu viditelného vložení zboží do košíku i v UI módu pro spuštění testu. Test procházel, avšak v UI nebylo vložení do košíku vidět. Test končil "loadingem".
+});
