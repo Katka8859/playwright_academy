d1469361550d86aae1e917335cf64fd9e00c4775
unknown <you@example.com>
2025-08-21 21:07:59 +0200

    add code from debbuging lection

---
+++ tests/learning/debugging/failing_tests.spec.ts
@@ -0,0 +1,22 @@
+/*import { expect, test } from "@playwright/test";
+
+test.describe("Failing tests", () => {
+  test("Failing action", async ({ page }) => {
+    await page.goto("http://tredgate.com/pmtool/");
+    await page.locator("#username111").fill("1234");
+  });
+
+  test("Failing assert", async ({ page }) => {
+    let someData;
+    await page.goto("http://tredgate.com/pmtool/");
+    someData = { user: "bestUser" };
+    await expect(
+      page.locator(".form-title"),
+      "Login Title have text"
+    ).toHaveText("Přihlášení");
+    someData = { user: "AnotherUser" };
+    console.log(someData);
+  });
+});*/
+
+// ctrl + shift + p reloud - developer reload window - používá se když něco svítí červeně a nemělo by třeba - tak je to dobrý pro refresh reload VS
