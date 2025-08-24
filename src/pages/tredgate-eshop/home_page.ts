d333c78ead3764ede54d40eee00dfbcfcef65bc1
unknown <you@example.com>
2025-08-24 22:53:04 +0200

    Create bonus homework - Asserts and hooks

--- src/pages/tredgate-eshop/home_page.ts
+++ src/pages/tredgate-eshop/home_page.ts
@@ -1,4 +1,4 @@
-import { Locator, Page } from "@playwright/test";
+import { expect, Locator, Page } from "@playwright/test";
 import { RegistrationPage } from "./registration_page.ts";
 
 export class HomePage {
@@ -6,12 +6,26 @@ export class HomePage {
   readonly url = "https://tredgate.com/eshop/";
   readonly myAccoutDropdownMenu: Locator;
   readonly registerDropdownMenu: Locator;
+  readonly logo: Locator;
+  readonly shoppingItemsButton: Locator;
+  readonly searchPlaceholder: Locator;
+  readonly featuredHeader: Locator;
+  readonly informationFooterSectionHeader: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.myAccoutDropdownMenu = page.locator("#top-links a i.fa-user");
     this.registerDropdownMenu = page.locator(
       ".dropdown-menu a[href='https://tredgate.com/eshop/index.php?route=account/register']"
+    );
+    this.logo = page.locator("//img[@title='Tredgate Obchod']");
+    this.shoppingItemsButton = page.locator("//span[@id='cart-total']");
+    this.searchPlaceholder = page.locator(
+      "[id='search'] [placeholder='Search']"
+    );
+    this.featuredHeader = page.locator("//h3[text()='Featured']");
+    this.informationFooterSectionHeader = page.locator(
+      "//h5[text()='Information']"
     );
   }
 
@@ -24,5 +38,36 @@ export class HomePage {
     await this.myAccoutDropdownMenu.click();
     await this.registerDropdownMenu.click();
     return new RegistrationPage(this.page);
+  }
+
+  async assertPageLogoIsVisible(): Promise<this> {
+    await expect(this.logo).toBeVisible();
+    return this;
+  }
+  async assertShoppingItemsButtonIsVisible(): Promise<this> {
+    await expect(this.shoppingItemsButton).toBeVisible();
+    return this;
+  }
+
+  async assertSearchPlaceholderExist(placeholderText: string): Promise<this> {
+    await expect(this.searchPlaceholder).toHaveAttribute(
+      "placeholder",
+      placeholderText
+    );
+    return this;
+  }
+
+  async assertFeaturedHeaderHasText(featuredText: string): Promise<this> {
+    await expect(this.featuredHeader).toHaveText(featuredText);
+    return this;
+  }
+
+  async assertInformationFooterSectionHeaderHasText(
+    informationText: string
+  ): Promise<this> {
+    await expect(this.informationFooterSectionHeader).toHaveText(
+      informationText
+    );
+    return this;
   }
 }
