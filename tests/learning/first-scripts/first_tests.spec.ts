7f1bc831de287574eeae13f4ecb0ab4b171067c5
unknown <you@example.com>
2025-09-09 20:57:35 +0200

    Add code from ddt lection

--- tests/learning/first-scripts/first_tests.spec.ts
+++ tests/learning/first-scripts/first_tests.spec.ts
@@ -52,3 +52,5 @@ příkaz pro spuštění terminálového reportéru: formou listu
 npx playwright test --reporter=list*/
 
 // ! kontrola regexu - jestli ten co nám vyplyvla AI, tak jestli funguje: regex101.com
+
+// ! http://day.js.org/ knihovna pro generování datumu
